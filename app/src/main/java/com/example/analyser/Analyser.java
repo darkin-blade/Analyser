269780e926a6762ee47316a945a6c8147aa566ed
Lynx <2104934038@qq.com>
2019-09-14 16:33:40 +0800

    todo:horizontal居中

--- app/src/main/java/com/example/analyser/Analyser.java
+++ app/src/main/java/com/example/analyser/Analyser.java
@@ -3,6 +3,7 @@ package com.example.analyser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Analyser extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Analyser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.cur_path);
+        textView.setSelected(true);
     }
 }
