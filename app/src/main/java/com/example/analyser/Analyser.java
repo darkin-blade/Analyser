60ddabd649167187e366f4ee3d8f58f37ae8ba75
Lynx <2104934038@qq.com>
2019-09-14 16:47:34 +0800

    createItem

--- app/src/main/java/com/example/analyser/Analyser.java
+++ app/src/main/java/com/example/analyser/Analyser.java
@@ -2,17 +2,103 @@ package com.example.analyser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class Analyser extends AppCompatActivity {
+    static int item_height = 130;
+    static int name_padding = 40;
+    static int type_padding = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.cur_path);
-        textView.setSelected(true);
+        readPath(getExternalFilesDir("").getAbsolutePath());// TODO app目录
+    }
+
+    public void readPath(String dirPath) {
+        if (dirPath == null) {
+            info(this, "can't access this path");
+            return;
+        }
+
+        LinearLayout layout = findViewById(R.id.item_list);
+        layout.removeAllViews();
+        createItem(2, "..", dirPath);// 父目录
+    }
+
+    static public void info(Context context, String log) {
+        Toast toast =  Toast.makeText(context, log, Toast.LENGTH_SHORT);
+        View view = toast.getView();
+        view.setBackgroundResource(R.drawable.toast);
+        TextView textView = view.findViewById(android.R.id.message);
+        textView.setTextColor(Color.rgb(0xff, 0xff, 0xff));
+        toast.show();
+    }
+
+    private LinearLayout createItem(int itemType, final String itemName, final String itemPath) {// 创建图标
+        LinearLayout layout = findViewById(R.id.item_list);
+        LinearLayout.LayoutParams itemParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, item_height);
+        LinearLayout.LayoutParams typeParam = new LinearLayout.LayoutParams(item_height, item_height);
+        LinearLayout.LayoutParams iconParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        LinearLayout.LayoutParams nameParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+
+        LinearLayout item = new LinearLayout(this);// TODO 参数
+        item.setLayoutParams(itemParam);
+        item.setBackgroundResource(R.color.grey);
+        item.setPadding(name_padding, 0, 0, 0);
+
+        LinearLayout type = new LinearLayout(this);// 图标的外圈
+        type.setLayoutParams(typeParam);
+        type.setPadding(type_padding, type_padding, type_padding, type_padding);
+
+        View icon = new View(this);// 图标
+        icon.setLayoutParams(iconParam);
+        if (itemType == 0) {// 文件
+            icon.setBackgroundResource(R.drawable.item_file);
+        } else {// 文件夹
+            icon.setBackgroundResource(R.drawable.item_dir);
+        }
+
+        TextView name = new TextView(this);// 文件名
+        name.setLayoutParams(nameParam);
+        name.setBackgroundResource(R.color.grey);
+        name.setText(itemName);
+        name.setPadding(name_padding, name_padding, name_padding, name_padding);
+        name.setSingleLine();
+
+        type.addView(icon);
+        item.addView(type);
+        item.addView(name);
+
+        if (itemType == 2) {// 父文件夹
+            item.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    File dir = new File(itemPath);
+                    readPath(dir.getParent());
+                }
+            });
+        } else if (itemType == 1) {// `点击`遍历子文件夹
+            item.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    readPath(itemPath + "/" + itemName);
+                }
+            });
+        }
+
+        layout.addView(item);
+
+        return item;
     }
 }
